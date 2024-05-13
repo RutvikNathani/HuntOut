644c4abda26e26fd0a0aa62cbf4580bb0582be7a
rutvik nathani <rutviknathani76218@gmail.com>
2024-05-13 09:37:54 +0530

    a

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,7 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = ''; // If using proxy
+export const BASE_URL =
+  'https://vercel.com/rutviks-projects-451e3582/final/6UJnFDFVhC9bkHSfT8RvRkSuDRQB'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
